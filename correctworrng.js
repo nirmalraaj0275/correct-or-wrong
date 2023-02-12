5e8919981ddcda2855d157b671c1d72de13c692b
Nirmal Raj <nirmalraaj0275@gmail.com>
2023-02-12 16:39:00 +0530

    Correct or wrong

---
+++ correctworrng.js
@@ -0,0 +1,55 @@
+function checkAnswer(){
+    const input = document.getElementById("input");
+    const status = document.getElementById("status");
+
+if(input.value=="tiger"){
+   status.innerText = "correct";
+}
+else{
+    status.innerText="wrong";
+}
+}
+function checkAnswer2(){
+    const input2 = document.getElementById("input2");
+    const status2 = document.getElementById("status2");
+
+if(input2.value=="peacock"){
+   status2.innerText = "correct";
+}
+else{
+    status2.innerText="wrong";
+}
+}
+function checkAnswer3(){
+    const input3 = document.getElementById("input3");
+    const status3 = document.getElementById("status3");
+
+if(input3.value=="mango tree"){
+   status3.innerText = "correct";
+}
+else{
+    status3.innerText="wrong";
+}
+}
+function checkAnswer4(){
+    const input4 = document.getElementById("input4");
+    const status4 = document.getElementById("status4");
+
+if(input4.value=="delhi"){
+   status4.innerText = "correct";
+}
+else{
+    status4.innerText="wrong";
+}
+}
+function checkAnswer5(){
+    const input5 = document.getElementById("input5");
+    const status5 = document.getElementById("status5");
+
+if(input5.value=="Narendra Modi"){
+   status5.innerText = "correct";
+}
+else{
+    status5.innerText="wrong";
+}
+}
